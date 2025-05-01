84b15b5eafdddc3cff07a31c7f537dc9793a1735
Chidubem Uchendu <133173187+Dubemxe@users.noreply.github.com>
2025-05-01 11:33:42 +0100

    Update ind_btn.js

--- static/ind_btn.js
+++ static/ind_btn.js
@@ -8,7 +8,7 @@ async function searchSong_onpage() {
         document.getElementById('searchMessage').textContent = `Here's the results for "${query}"`;
         
         // Personal nocodeapi url
-        const searchUrl = `https://v1.nocodeapi.com/jmusic3/spotify/webvybGfTcWRBfis/search?q=${encodeURIComponent(query)}&type=track`;
+        const searchUrl = `https://v1.nocodeapi.com/jmusic6/spotify/lQcmZrFNcMqfCRzc/search?q=${encodeURIComponent(query)}&type=track`;
 
         const response = await fetch(searchUrl);
 

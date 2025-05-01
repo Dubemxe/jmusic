8a0e2c597fccd59441b13cd1017d0dec9b475e36
Chidubem Uchendu <133173187+Dubemxe@users.noreply.github.com>
2025-05-01 11:29:20 +0100

    Update search_artists.js

--- static/search_artists.js
+++ static/search_artists.js
@@ -5,7 +5,7 @@ async function getTrackIdsByArtist(artistName) {
     const encodedArtistName = encodeURIComponent(artistName);
 
     // Construct the search URL for tracks by the artist
-    const searchUrl = `https://v1.nocodeapi.com/jmusic5/spotify/ivEUaoclsKKyrlqW/search?q=${encodedArtistName}&type=track`;
+    const searchUrl = `https://v1.nocodeapi.com/jmusic6/spotify/lQcmZrFNcMqfCRzc/search?q=${encodedArtistName}&type=track`;
 
     // Make a GET request to the search URL with the Spotify API access token
     const response = await fetch(searchUrl);
@@ -178,7 +178,7 @@ async function searchArtist() {
       return;
   }
   // Spotify API URL for searching artists
-  const searchUrl = `https://v1.nocodeapi.com/jmusic5/spotify/ivEUaoclsKKyrlqW/search?q=${encodeURIComponent(artistQuery)}&type=artist`;
+  const searchUrl = `https://v1.nocodeapi.com/jmusic6/spotify/lQcmZrFNcMqfCRzc/search?q=${encodeURIComponent(artistQuery)}&type=artist`;
   // Fetch the artist's data
   try {
       const response = await fetch(searchUrl);

6c0571a19ff90b88f9b9b81d0a9ffe14b4806396
Chidubem Uchendu <133173187+Dubemxe@users.noreply.github.com>
2025-08-23 12:58:55 +0100

    code clean up

--- static/mysearchpage.js
+++ static/mysearchpage.js
@@ -177,32 +177,7 @@ async function searchSong() {
          // Store the search results in sessionStorage
         sessionStorage.setItem('searchResults', JSON.stringify(trackItems));
              // Redirect to the results page
-        window.location.href = 'search_results.html';/*
-        const songListHTML = trackItems.map((track, index) =>  {
-            const artists = track.artists.map(artist => artist.name).join(', ');
-            const trackHTML = `
-        <div class="musicInfo" onclick="getAudio('${track.preview_url}')">
-        <img src="${track.album.images[0].url}" alt="${track.name}" class="albumImage" id="popup_image${index}" onclick="popupDiv(${index})">
-        <div id="content_div${index}" class="content_div">
-        <div class="img_box">
-         <img src="${track.album.images[0].url}" alt="${track.name}" class="pp_img">
-        <button class="xbtn" onclick="popupDiv(${index})"><img src="styles/images/icons8-x-50 (1).png" class="xicon"></button>
-        </div>
-        <p class="p1">${track.name}</p>
-        <p class="dp">${artists} </p>
-        <p class="dp">Released on ${track.album.release_date}</p>
-        <p class="dp1">From the ${track.album.name} Album</p>
-        </div>
-        <p class="artistName">${artists} </p>
-        <p class="songTitle"> - ${track.name}</p>
-        <p class="duration">${msToTime(track.duration_ms)}</p>
-        ${track.preview_url ? `<p> </p>` : `<p class="noPreview">No Preview Available</p>`}
-        </div>
-        `;
-          return trackHTML;
-        }).join('');
-
-        document.getElementById('songList').innerHTML = songListHTML;*/
+        window.location.href = 'search_results.html';
 
         } catch (error) {
                 console.error('Error searching for the song:', error);

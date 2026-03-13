7742e5594e76af6582cf4e007ace41a786bc4871
Chidubem Uchendu <133173187+Dubemxe@users.noreply.github.com>
2026-03-13 14:37:49 -0400

    Add discoverArtists array for artist carousel
    Added a list of artists with images to the carousel.

--- static/get_info.js
+++ static/get_info.js
@@ -250,3 +250,42 @@ function rotateCards(){
 
 setInterval(rotateCards,3500);
 
+const discoverArtists = [
+  {
+    name: "Drake",
+    image: "styles/images/drake.jpg"
+  },
+  {
+    name: "Wizkid",
+    image: "styles/images/wizkid.jpg"
+  },
+  {
+    name: "Burna Boy",
+    image: "styles/images/burna.jpg"
+  },
+  {
+    name: "Future",
+    image: "styles/images/future.jpg"
+  },
+  {
+    name: "Tems",
+    image: "styles/images/tems.jpg"
+  }
+];
+
+const carousel = document.getElementById("artistCarousel");
+
+discoverArtists.forEach(artist => {
+
+  const card = document.createElement("div");
+  card.className = "artist-card-stack";
+
+  card.innerHTML = `
+    <img src="${artist.image}" alt="${artist.name}">
+    <p>${artist.name}</p>
+  `;
+
+  carousel.appendChild(card);
+
+});
+

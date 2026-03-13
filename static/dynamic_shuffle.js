f21a55252f83887d848d6f5fd97094e1db6810f5
Chidubem Uchendu <133173187+Dubemxe@users.noreply.github.com>
2026-03-13 14:55:56 -0400

    Update dynamic_shuffle.js

--- static/dynamic_shuffle.js
+++ static/dynamic_shuffle.js
@@ -36,3 +36,32 @@ discoverArtists.forEach(artist => {
   carousel.appendChild(card);
 
 });
+
+const cards = document.querySelectorAll(".artist-card-stack");
+
+let positions = ["left","center","right"];
+
+function updatePositions(){
+
+    cards.forEach((card,i)=>{
+
+        card.classList.remove("left","center","right");
+
+        card.classList.add(positions[i]);
+
+    });
+
+}
+
+updatePositions();
+
+function rotateCards(){
+
+    positions.unshift(positions.pop());
+
+    updatePositions();
+
+}
+
+setInterval(rotateCards,3500);
+

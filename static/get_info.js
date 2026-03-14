28dad6283ef694771308de76599cb15e97a11999
Chidubem Uchendu <133173187+Dubemxe@users.noreply.github.com>
2026-03-13 20:28:10 -0400

    Update get_info.js

--- static/get_info.js
+++ static/get_info.js
@@ -9,28 +9,7 @@ const data = await response.json();
 return data.token;
 
 }
-function popupaDiv(){
 
-const panel = document.getElementById("searchbackDiv");
-const overlay = document.getElementById("searchOverlay");
-
-panel.classList.toggle("active");
-overlay.classList.toggle("active");
-
-}
-document
-.getElementById("searchOverlay")
-.addEventListener("click", () => {
-
-document
-.getElementById("searchbackDiv")
-.classList.remove("active");
-
-document
-.getElementById("searchOverlay")
-.classList.remove("active");
-
-});
 function changeUrl(url) {
                 window.location.href = url;
                 }

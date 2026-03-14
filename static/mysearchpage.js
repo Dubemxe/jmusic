2801b0b5e099af8a54cb99378df8cb58baa7fe3b
Chidubem Uchendu <133173187+Dubemxe@users.noreply.github.com>
2026-03-14 02:23:19 -0400

    Update mysearchpage.js

--- static/mysearchpage.js
+++ static/mysearchpage.js
@@ -1,347 +1,301 @@
-const fullPlayer = document.getElementById('fullPlayer');
-const closeBtn = document.getElementById('closeBtn');
-const openBtn = document.getElementById('openPlayerBtn');
-const container = document.querySelector('.container');
-const openFp = document.getElementById('ofp');
+const songList = document.getElementById("songList")
+const fullPlayer = document.getElementById("fullPlayer")
+
+const albumArt = document.getElementById("albumArt")
+const songTitle = document.getElementById("songTitle")
+const songArtist = document.getElementById("songArtist")
+const songDuration = document.getElementById("songDuration")
+const currentTime = document.getElementById("currentTime")
+
+const playPauseBtn = document.getElementById("playPauseBtn")
+const favBtn = document.getElementById("favBtn")
+const closeBtn = document.getElementById("closeBtn")
+
+let currentSong = null
+let isPlaying = false
 
 
-function openFullPlayer() {
-  fullPlayer.classList.add('show');
-}
 
-function closeFullPlayer() {
-  fullPlayer.classList.remove('show');
+// OPEN PLAYER
 
-  const leftSide = document.getElementById('leftside');
-  const searchBar = document.getElementById('searchbar');
-  const accImg = document.getElementById('accimg');
+function openFullPlayer(){
 
-  searchBar.style.width = "800px";
-  leftSide.style.width = "1241px";
-  accImg.style.marginLeft = "150px";
-  accImg.style.marginRight = "0";
-}
-
-function adjustSize() {
-  const leftSide = document.getElementById('leftside');
-  const searchBar = document.getElementById('searchbar');
-  const searchBtn = document.getElementsByClassName('searchBtn')
-
-  searchBar.style.width = "300px";
-  leftSide.style.width = "700px";
-  searchBtn.style.marginRight = "30px";
+fullPlayer.classList.add("show")
 
 }
-closeBtn.addEventListener('click', closeFullPlayer);
 
-const songImage = document.getElementById('songImage');
-const songTitle = document.getElementById('songTitle');
-const songArtist = document.getElementById('songArtist');
-const songDuration = document.getElementById('songDuration');
-const currentTime = document.getElementById('currentTime');
 
-const playPauseBtn = document.getElementById('playPauseBtn');
-const favBtn = document.getElementById('favBtn');
-const shuffleBtn = document.getElementById('shuffleBtn');
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
+// CLOSE PLAYER
 
-let isPlaying = false;
-let isFavorited = false;
+function closeFullPlayer(){
 
-// Simulated song object
-const sampleSong = {
-  title: "Lose Yourself",
-  artist: "Eminem",
-  duration: "5:26",
-  artistImage: "https://i.scdn.co/image/ab67616d0000b273dff7e6223f9a7ff3bce8aa55"
-};
+fullPlayer.classList.remove("show")
 
-// Initialize player
-function showPlayer(song) {
-  songTitle.textContent = song.title;
-  songArtist.textContent = song.artist;
-  songDuration.textContent = song.duration;
-  currentTime.textContent = "1:23"; // Replace with real time tracking
-
-  fullPlayer.classList.remove('hidden');
 }
 
-// Controls
-playPauseBtn.addEventListener('click', () => {
-  isPlaying = !isPlaying;
-  playPauseBtn.innerHTML = isPlaying
-  ? '<img src="images/icons8-pause-64 (1).png" class="ppnp" alt="Pause">'
-  : '<img src="images/icons8-play-50.png" class="ppnp" alt="Play">';
-});
-
-favBtn.addEventListener('click', () => {
-  isFavorited = !isFavorited;
-  favBtn.classList.toggle('active');
-  favBtn.textContent = isFavorited ? '❤️' : '♡';
-});
-
-closeBtn.addEventListener('click', () => {
-  fullPlayer.classList.add('hidden');
-});
-
-shuffleBtn.addEventListener('click', () => {
-  alert("Shuffle toggled!");
-});
-
-prevBtn.addEventListener('click', () => {
-  alert("Previous track");
-});
-
-nextBtn.addEventListener('click', () => {
-  alert("Next track");
-});
-
-// Launch player
-showPlayer(sampleSong);
+closeBtn.addEventListener("click",closeFullPlayer)
 
 
-// When user clicks the close (X) button
-closeBtn.addEventListener('click', () => {
-  fullPlayer.style.display = 'none';
-  container.classList.add('expanded');
-  openBtn.classList.remove('hidden');
-});
 
-// When user clicks the "Now Playing" button
-openBtn.addEventListener('click', () => {
-  fullPlayer.style.display = 'flex';
-  container.classList.remove('expanded');
-  openBtn.classList.add('hidden');
-});
+// PLAY AUDIO
+
+function playAudio(url){
+
+if(!url){
+
+alert("No preview available")
+
+return
+
+}
+
+if(currentSong){
+
+currentSong.pause()
+
+}
+
+currentSong = new Audio(url)
+
+currentSong.play()
+
+isPlaying = true
+
+playPauseBtn.innerHTML =
+'<img src="styles/images/icons8-pause-64.png" class="ppnp">'
+
+}
 
 
-function changeUrl(url) {
-  window.location.href = url;
+
+// PLAY / PAUSE BUTTON
+
+playPauseBtn.addEventListener("click",()=>{
+
+if(!currentSong) return
+
+if(isPlaying){
+
+currentSong.pause()
+
+isPlaying=false
+
+playPauseBtn.innerHTML =
+'<img src="styles/images/icons8-play-50.png" class="ppnp">'
+
+}else{
+
+currentSong.play()
+
+isPlaying=true
+
+playPauseBtn.innerHTML =
+'<img src="styles/images/icons8-pause-64.png" class="ppnp">'
+
+}
+
+})
+
+
+
+// FAVORITE BUTTON
+
+let isFav=false
+
+favBtn.addEventListener("click",()=>{
+
+isFav=!isFav
+
+favBtn.textContent = isFav ? "❤️" : "♡"
+
+})
+
+
+
+// TIME FORMAT
+
+function msToTime(duration){
+
+const minutes = Math.floor(duration/60000)
+
+const seconds = ((duration%60000)/1000).toFixed(0)
+
+return minutes+":"+(seconds<10?"0":"")+seconds
+
+}
+
+
+
+// LOAD SONG DATA
+
+function loadPlayer(track){
+
+albumArt.innerHTML =
+`<img src="${track.album.images[0].url}" class="player-art">`
+
+songTitle.textContent = track.name
+
+songArtist.textContent =
+track.artists.map(a=>a.name).join(", ")
+
+songDuration.textContent =
+msToTime(track.duration_ms)
+
+}
+
+
+
+// RENDER SONGS
+
+function renderSongs(tracks){
+
+songList.innerHTML=""
+
+tracks.forEach((track,index)=>{
+
+const artists =
+track.artists.map(a=>a.name).join(", ")
+
+const songHTML = `
+
+<div class="musicInfo">
+
+<img
+src="${track.album.images[0].url}"
+class="albumImage"
+
+onclick="
+loadPlayer(${index});
+openFullPlayer();
+playAudio('${track.preview_url}')
+">
+
+<div class="artistsDets">
+
+<p class="songTitle">${track.name}</p>
+
+<p class="artistName">${artists} · ${track.album.name}</p>
+
+</div>
+
+<p class="duration">
+${msToTime(track.duration_ms)}
+</p>
+
+</div>
+
+`
+
+songList.innerHTML += songHTML
+
+})
+
+}
+
+
+
+// SEARCH SONGS
+
+async function searchSong_onpage(){
+
+try{
+
+const query =
+document.getElementById("artistSearchInput")
+.value.trim()
+
+if(!query) return
+
+const url =
+`https://v1.nocodeapi.com/jmusicdm/spotify/KAIfcEwQNVgCQtXA/search?q=${encodeURIComponent(query)}&type=track`
+
+const res = await fetch(url)
+
+const data = await res.json()
+
+const tracks = data.tracks.items
+
+renderSongs(tracks)
+
+}catch(error){
+
+console.error(error)
+
+}
+
+}
+
+
+
+// LOAD FROM HOMEPAGE TRACK IDS
+
+function getTrackIdsFromHash(){
+
+const hash = window.location.hash
+
+const match = hash.match(/trackIds=([^&]*)/)
+
+if(match){
+
+return decodeURIComponent(match[1]).split(",")
+
+}
+
+return []
+
+}
+
+
+
+async function loadTracksFromIds(ids){
+
+try{
+  const token = await getSpotifyToken();
+  
+  const searchResponse = await fetch(
+  `https://api.spotify.com/v1/search?q=${encodeURIComponent(artistName)}&type=artist&limit=1`,
+  {
+  headers:{
+  Authorization:`Bearer ${token}`
   }
-let currentSong = null; //stores the current song being played
+  });
+  
+  const searchData = await searchResponse.json();
 
-function playAudio(url) {
-    if (currentSong) {
-            currentSong.pause(); // pause the song playing
-            //currentSong.currentTime = 0; // resets the song to the start
-    }
-    currentSong = new Audio(url);
-    currentSong.play(); //play the song clicked
-}
-function getAudio(previewUrl) {
-         if (previewUrl) {
-                 playAudio(previewUrl);
-         } else { console.log("No audio for this track");
-         }
-}
-function pauseAudio() {
-    if (currentSong) {
-        currentSong.pause(); // Pause the current audio
-    }
-}
-function addFont() {
-    const fontLink = document.createElement('link');
-    fontLink.href = 'https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap';
-    fontLink.rel = 'stylesheet';
-    document.head.appendChild(fontLink);
-}
-addFont();
-// Search function to retrieve music data from the spotify api
-async function searchSong() {
-    try {
-        const query = document.getElementById('searchQuery').value.trim();
-        if (!query) {
-            console.log('Please enter a song title');
-           return;
-        }
-        // Store the query in sessionStorage to use on the results page
-        sessionStorage.setItem('searchQuery', query);
-        
-        // Personal nocodeapi url
-        const searchUrl = `https://v1.nocodeapi.com/jmusicdm/spotify/KAIfcEwQNVgCQtXA/search?q=${encodeURIComponent(query)}&type=track`;
+renderSongs(searchData.tracks)
 
-        const response = await fetch(searchUrl);
+}catch(error){
 
-        if (!response.ok) {
-            throw new Error('Failed to search for the song'); 
-        }
+console.error(error)
 
-        const data = await response.json();
-         const trackItems = data.tracks.items;
-
-        if (trackItems.length === 0) {
-            console.log('No songs found');
-            return;
-        }
-         // Store the search results in sessionStorage
-        sessionStorage.setItem('searchResults', JSON.stringify(trackItems));
-             // Redirect to the results page
-        window.location.href = 'search_results.html';
-
-        } catch (error) {
-                console.error('Error searching for the song:', error);
-    }
 }
 
-async function getTrackIdByArtist(artistName) {
-  try {
-    // Encode the artist name for use in the URL
-    const encodedArtistName = encodeURIComponent(artistName);
-
-    // Construct the search URL for tracks by the artist
-    const searchUrl = `https://v1.nocodeapi.com/jmusicdm/spotify/KAIfcEwQNVgCQtXA/search?q=${encodedArtistName}&type=track`;
-
-    // Make a GET request to the search URL with the Spotify API access token
-    const response = await fetch(searchUrl);
-
-    // Check if the response is successful
-    if (!response.ok) {
-      throw new Error('Failed to fetch tracks by artist');
-    }
-
-    // Parse the response JSON
-    const data = await response.json();
-
-    // Extract the track IDs from the response
-    let trackIds = data.tracks.items.map(item => item.id);
-
-    trackIds = trackIds.slice(0, 5);
-
-    // Return the array of track IDs
-    return trackIds;
-  } catch (error) {
-    console.error('Error fetching track IDs by artist:', error);
-     return [];
-  }
 }
 
-document.getElementById('searchQuery').addEventListener('keydown', function(event) {
-  if (event.key === 'Enter') {
-           document.getElementById('sBtn').click();  // Trigger search button click
-  }
-});
 
-function msToTime(duration) {
-  const minutes = Math.floor(duration / 60000);
-  const seconds = ((duration % 60000) / 1000).toFixed(0);
-  return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+
+// PAGE LOAD
+
+const ids = getTrackIdsFromHash()
+
+if(ids.length>0){
+
+loadTracksFromIds(ids)
+
 }
 
-async function getSongInform(trackIds) {
-try {
-        const trackIdsStr = trackIds.join(',');
-        const trackInfoUrl = `https://v1.nocodeapi.com/jmusicdm/spotify/KAIfcEwQNVgCQtXA/tracks?ids=${trackIdsStr}`;
-        const response = await fetch(trackInfoUrl);
 
-        // Check if the response is OK
-        if (!response.ok) {
-            throw new Error('Failed to fetch song info');
-        }
-  // Parse the response JSON
-        const data = await response.json();
 
-    data.tracks.forEach((track, index) => {
-        const songHTML = `
-         <div class="musicInfo" onclick="getAudio('${track.preview_url}')" class="musicInfo" style="font-family: 'Manrope', sans-serif;">
-        <img src="${track.album.images[0].url}" alt="${track.name}" class="albumImage" id="popup_image${index}" onclick="popupDiv(${index})">
-        <div class="artistsDets">
-        <p class="songTitle" style="font-family: 'Manrope'">${track.name}</p>
-        <p class="artistName" style="font-family: 'Manrope'">${track.artists.map(artist => artist.name).join(', ')} . ${track.album.name}</p>
-        </div>
-        <p class="duration" style="font-family: 'Manrope'">${msToTime(track.duration_ms)}</p> 
-        <div id="content_div${index}" class="content_div">
-        <div class="img_box">
-        <img src="${track.album.images[0].url}" alt="${track.name}" class="pp_img">
-        <button class="xbtn" onclick="popupDiv(${index})"><img src="styles/images/icons8-x-50 white.png" class="xicon"></button>
-        </div>
-        <p class="p1" style="font-family: 'Manrope'">${track.name}</p>
-        <p class="dp" style="font-family: 'Manrope'">${track.artists.map(artist => artist.name).join(', ')} </p>
-        <p class="dp" style="font-family: 'Manrope'">Released on ${track.album.release_date}</p>
-        <p class="dp" style="font-family: 'Manrope'">From the ${track.album.name} Album</p>
-        </div>
-         ${track.preview_url ? `<p> </p>` : `<p class="noPreview" style="font-family: 'Manrope'">No Audio Available</p>`}
-        </div>
-        `;
-        document.getElementById('songList').innerHTML += songHTML;
-    });
-   } catch (error) {
-        console.error('Error fetching song info:', error);
-    }
+// ENTER KEY SEARCH
+
+document
+.getElementById("artistSearchInput")
+.addEventListener("keydown",(e)=>{
+
+if(e.key==="Enter"){
+
+searchSong_onpage()
+
 }
 
-function popupDiv(index) {
-
-  const contentDiv = document.getElementById(`content_div${index}`);
-
-  // Toggle the visibility of the div
-  if (contentDiv.style.display === 'none' || contentDiv.style.display === '') {
-    contentDiv.style.display = 'block'; // Show the div
-  } else {
-    contentDiv.style.display = 'none'; // Hide the div
-  }
-}
-
-// Function to extract trackIds from the URL hash
-function getTrackIdsFromHash() {
-    const hash = window.location.hash;
-
-    // Check if the hash contains the trackIds parameter
-    const trackIdsParam = hash.match(/trackIds=([^&]*)/);
-    if (trackIdsParam && trackIdsParam[1]) {
-        // Decode the track IDs and split them into an array
-        return decodeURIComponent(trackIdsParam[1]).split(',');
-    }
-  return [];
-}
-
-// Example: Fetch the track IDs when the page loads
-const trackIds = getTrackIdsFromHash();
-if (trackIds.length > 0) {
-    // Use the trackIds to fetch song information
-    console.log(trackIds);
-    getSongInform(trackIds);
-}
-
-window.onload = function() {
-    const searchResults = JSON.parse(sessionStorage.getItem('searchResults'));
-
-    if (!searchResults) {
-        console.log('No search results found');
-        return;
-    }
-
-    const resultsContainer = document.getElementById('ResSong_list');
-    searchResults.forEach((track, index) => {
-        const artists = track.artists.map(artist => artist.name).join(', ');
-        const trackHTML = `
-                 <div class="musicInfo" onclick="getAudio('${track.preview_url}')" style="font-family: 'Manrope'">
-        <img src="${track.album.images[0].url}" alt="${track.name}" class="albumImage" id="popup_image${index}" onclick="popupDiv(${index})">
-         <div class="artistsDets">
-        <p class="songTitle" style="font-family: 'Manrope'">${track.name}</p>
-        <p class="artistName" style="font-family: 'Manrope'">${track.artists.map(artist => artist.name).join(', ')} . ${track.album.name}</p>
-        </div>
-        <p class="duration" style="font-family: 'Manrope'">${msToTime(track.duration_ms)}</p>
-                <div id="content_div${index}" class="content_div">
-        <div class="img_box">
-         <img src="${track.album.images[0].url}" alt="${track.name}" class="pp_img">
-        <button class="xbtn" onclick="popupDiv(${index})"><img src="styles/images/icons8-x-50 white.png" class="xicon"></button>
-        </div>
-        <p class="p1" style="font-family: 'Manrope'">${track.name}</p>
-        <p class="dp" style="font-family: 'Manrope'">${artists} </p>
-        <p class="dp" style="font-family: 'Manrope'">Released on ${track.album.release_date}</p>
-        <p class="dp1" style="font-family: 'Manrope'">From the ${track.album.name} Album</p>
-        </div>
-        ${track.preview_url ? `<p> </p>` : `<p class="noPreview">No Preview Available</p>`}
-            </div>
-        `;
-        resultsContainer.innerHTML += trackHTML;
-    });
-}
- // Retrieve the query from sessionStorage
-    const searchQuery = sessionStorage.getItem('searchQuery');
-
-    // Display the message on the results page
-    if (searchQuery) {
-        document.getElementById('searchMessage').textContent = `Here's the result for "${searchQuery}"`;
-    }
+})
